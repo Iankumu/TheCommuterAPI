fe85170998482ecfaedb2ae666ad845c5f9e30be
iankumu <ikariuki741>
2020-07-29 16:25:46 +0300

    Small Fix

--- resources/views/success_email_verify.blade.php
+++ resources/views/success_email_verify.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Forgot Password Form</title>
+    <title>Email Verification Success</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
